7ad9f0dc9e19fd8796b877042e044996ba5f8757
ExilAurora <contact@exilaurora.ru>
2025-10-09 21:49:07 +0500

    Убралл ненужный роутер

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,12 +1,11 @@
 from aiogram import Dispatcher
-from . import start, schedule, main_buttons_handlers, test, group_register
+from . import start, main_buttons_handlers, test, group_register
 
 def register_handlers(dp: Dispatcher):
     """
     Регистрирует все роутеры бота.
     """
     dp.include_router(start.router)
-    dp.include_router(schedule.router)
     dp.include_router(main_buttons_handlers.router)
     dp.include_router(test.router)
 
