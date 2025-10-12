dd8bdef3b4254e2c28b64e6d23040008e496c8b6
ExilAurora <contact@exilaurora.ru>
2025-10-12 19:36:28 +0500

    Inline query поиск других групп
    Возможность искать расписание для других групп черерз inline query

--- src/handlers/inline_schedue.py
+++ src/handlers/inline_schedue.py
@@ -10,18 +10,19 @@ router = Router()
 @router.inline_query()
 async def inline_query_handler(query: InlineQuery, state: FSMContext, api: RusoilSafeAPI):
     results = []
+    text = query.query.strip()
+
+    now, now_cache = await api.GetNow() # get_now_safe(api)
+    if not now:
+        query.answer(results, cache_time=60, is_personal=True)
+        return
 
     data = await state.get_data()
     # Если бот настроен у пользователя
-    if data.get("group"):
+    if data.get("group") and not (text and len(text) > 2):
         group = data["group"]
         subgroup = data.get("subgroup", 0)
-        now, now_cache = await api.GetNow() # get_now_safe(api)
 
-        if not now:
-            query.answer(results, cache_time=60, is_personal=True)
-            return
-        
         days, days_from_cache = await api.GetSchedule(group, now.week_number) # get_schedule_safe(api, group, now.week_number)
 
         if not days:
@@ -57,7 +58,45 @@ async def inline_query_handler(query: InlineQuery, state: FSMContext, api: Rusoi
             )
         )
 
-    if query.query:
+    if text and len(text) > 2:
+        groups, _ = await api.GetGroups(text, cache_ttl=60**2)
+        if groups:
+            curID = 0
+            for i in range(0, len(groups[:18])):
+                try:
+                    group = groups[i].name
+                    days, days_from_cache = await api.GetSchedule(groups[i].name, now.week_number, 300)
+                    subgroup=1
+
+                    today_obj = next((d for d in days if d.day_of_week == now.day_of_week), None)
+                    todayRaspText = render_schedule_text(group, today_obj, days_names[now.day_of_week], subgroup, True if days_from_cache or now_cache else False)
+
+
+                    tommorow_obj = next((d for d in days if d.day_of_week == (now.day_of_week + 1 if now.day_of_week < 7 else 1)), None)
+
+                    tommorowRaspText = render_schedule_text(group, tommorow_obj, days_names[now.day_of_week + 1 if now.day_of_week < 7 else 1], subgroup, True if days_from_cache or now_cache else False)
+
+                    results.append(InlineQueryResultArticle(
+                        id = str(curID),
+                        title = f"Расписание на сегодня для {group}",
+                        input_message_content = InputTextMessageContent(
+                            message_text=todayRaspText
+                        ),
+                        description = "Вывести расписание на сегодня"
+                        )
+                    )
+                    results.append(InlineQueryResultArticle(
+                        id = str(curID + 1),
+                        title = f"Расписание на завтра для {group}",
+                        input_message_content = InputTextMessageContent(
+                            message_text=tommorowRaspText
+                        ),
+                        description = "Вывести расписание на завтра"
+                        )
+                    )
+                    curID += 2
+                except:
+                    pass
 
         pass
 
