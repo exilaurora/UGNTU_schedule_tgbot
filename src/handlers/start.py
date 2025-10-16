56d593e623ce7807f0081b98c19a28b894db649e
ExilAurora <contact@exilaurora.ru>
2025-10-16 23:44:58 +0500

    Added start message
    Теперь пользователи смогут узнать о возможностях бота при написании /start

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -12,6 +12,22 @@ async def command_start(message: Message, state: FSMContext):
     user = message.from_user
     if not user:
         return
+    
+    await message.answer(f"""Привет! Я помогу следить тебе за расписанием.
+                         
+Я буду работать даже если API УГНТУ недоступен, ведь я поддерживаю кэширование данных.
 
-    await message.answer("Привет! Введи свою группу", reply_markup=ReplyKeyboardRemove())
+Я поддерживаю inline query. просто напиши {html.code('@ugntu_raspbot')} в любом чате.
+Можно написать название группы, если нужно расписание другой группы.
+
+Ты можешь отслеживать расписание сразу нескольких групп.
+Достаточно запросить расписание, а затем сменить свою группу.
+Сообщение, которое я отправлю после первого запроса будет актуально для той группы, которую ты запрашивал.
+
+Даже при недоступности интернета ты не останешься без расписания, ведь telegram кэширует сообщения!
+
+Начнем настройку!
+""")
+
+    await message.answer("Введи свою группу", reply_markup=ReplyKeyboardRemove())
     await state.set_state(UserState.waiting_for_group)
